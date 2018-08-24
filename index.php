d579f071a4f793900bf2fbd87d21b2a810c455b5
Sterenger <salah@teclan.com.br>
2018-08-24 15:16:34 -0300

    init

--- index.php
+++ index.php
@@ -7,11 +7,15 @@ use Goutte\Client;
 $url = "http://www.sciencenews.org/topic/life-evolution";
 $client = new Client();
 $crawler = $client->request('GET',$url);
+echo "<center><h1> LIFE & EVOLUTION </h1></center";
 $crawler->filter('li.subtopic-listing-3-items')->each(function ($node, $index) {
 $imgSrc = $node->filter("img")->attr('data-echo');
 $category = $node->filter("span.field-content")->text();
 $title = $node->filter("div.views-field-title > span.field-content")->text();
-  echo "<img src='$imgSrc' /><br>";
+$url = $node->filter("div.views-field-title > span.field-content > a")->attr('href');
+  echo "<center> <img src='$imgSrc' /><br>";
   echo "Category : " . $category."<br>";
-  echo "Title : " . $title."<br>";
+  echo "Title : " . $title."<br> </center>";
+  echo $url;
 });
+
